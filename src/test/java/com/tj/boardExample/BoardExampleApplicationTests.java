cefb54da93d525e8c4976c7cf28181e9481401f2
hsyoo <yhs72358@naver.com>
2023-04-26 22:09:35 +0900

    Project INIT

---
+++ src/test/java/com/tj/boardExample/BoardExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tj.boardExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
